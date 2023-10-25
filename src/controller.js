ba37753fb8372a353c439d0bc9b9f20421637347
Matias Leonel Ramirez <matt.neetz.dev.@gmail.com>
2023-10-25 19:27:53 -0300

    Get One Get All

--- src/controller.js
+++ src/controller.js
@@ -1,22 +1,40 @@
 import {pool} from './database.js';
 
-class LibroController {
-    async getAll(req,res) {
-        const [result] = await pool.query('SELECT * FROM libros');
-        res.json(result);
+class LibrosController {
+    async getAll(req, res){
+        try {
+            const [result] = await pool.query('SELECT * FROM libros');
+            res.json(result);
+        } catch (error){
+            res.status(500).json({"Error": "Ocurrio un error al obtener los libros"});
+        }
+    }
+    
+    async getOne(req, res) {
+        try {
+            const libro = req.body;
+            const [result] = await pool.query('SELECT * FROM libros WHERE ISBN = ?', [libro.ISBN]);
+            if (result.length > 0) {
+                res.json(result[0]);
+            } else {
+                res.status(404).json({"Error": "No se encontro el libro con el ISB ${libro.ISBN}"});
+            }
+        } catch (error) {
+            res.status(500).json({"Error": "Ocurrio un error al obtener el libro"});
+        }
     }
 
-    async getOne(req,res){
-        const id= req.body.id;
-        const [resulto] = await pool.query('SELECT * FROM libros WHERE id = ?', [id]);
-        if (result.length > 0) {
-            
-            res.json(result[0]);
-        } else {
-            res.status(404).json({"Error": 'No se encontro el libro con el id ${id}'});
+    async add(req, res) {
+        try {
+            const libro = req.body;
+            const[result] = await pool.query(`INSERT INTO Libros(nombre, autor, categoria, aniopublicacion, ISBN) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.aniopublicacion, libro.ISBN]);
+            res.json({"ID insertado": result.insertId, "message": "Libro insertado exitosamente"});
+        } catch (error) {
+            res.status(500).json({"Error": "Ocurrio un error al agregar el libro"});
         }
     }
 }
 
 
-export const libro = new libro.LibroController();
+
+export const libro = new libro.LibrosController();
